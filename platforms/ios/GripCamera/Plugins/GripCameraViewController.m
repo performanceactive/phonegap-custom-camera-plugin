95a244ac92b16f80905030fd8224e1a275f202ab
Chris van Es <cvanes@gmail.com>
2014-02-25 11:32:43 +0000

    Adding activity indicator for iOS

--- platforms/ios/GripCamera/Plugins/GripCameraViewController.m
+++ platforms/ios/GripCamera/Plugins/GripCameraViewController.m
@@ -17,6 +17,7 @@
     AVCaptureDevice *_rearCamera;
     AVCaptureStillImageOutput *_stillImageOutput;
     UIButton *_captureButton;
+    UIActivityIndicatorView *_activityIndicator;
 }
 
 static const CGFloat kButtonWidth = 50;
@@ -32,25 +33,6 @@ static const CGFloat kVerticalInset = 30;
         _callback = callback;
         _captureSession = [[AVCaptureSession alloc] init];
         _captureSession.sessionPreset = AVCaptureSessionPresetHigh;
-        for (AVCaptureDevice *device in [AVCaptureDevice devices]) {
-            if ([device hasMediaType:AVMediaTypeVideo]) {
-                if ([device position] == AVCaptureDevicePositionBack) {
-                    _rearCamera = device;
-                    [_rearCamera lockForConfiguration:nil];
-                    if ([_rearCamera isTorchModeSupported:AVCaptureTorchModeAuto]) {
-                        _rearCamera.torchMode = AVCaptureTorchModeAuto;
-                    } else if ([_rearCamera isFlashModeSupported:AVCaptureFlashModeAuto]) {
-                        _rearCamera.flashMode = AVCaptureFlashModeAuto;
-                    }
-                    [_rearCamera unlockForConfiguration];
-                }
-            }
-        }
-        AVCaptureDeviceInput *cameraInput = [AVCaptureDeviceInput deviceInputWithDevice:_rearCamera error:nil];
-        [_captureSession addInput:cameraInput];
-        _stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
-        [_captureSession addOutput:_stillImageOutput];
-        [_captureSession startRunning];
     }
     return self;
 }
@@ -60,12 +42,25 @@ static const CGFloat kVerticalInset = 30;
     [_captureSession stopRunning];
 }
 
+- (void)observeValueForKeyPath:(NSString*)keyPath ofObject:(id)object change:(NSDictionary*)change context:(void*)context {
+    if ([keyPath isEqualToString:@"adjustingFocus"]) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self enableButtonIfCameraIsInFocus];
+        });
+    }
+}
+
 - (void)loadView {
     self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.view.backgroundColor = [UIColor blackColor];
     AVCaptureVideoPreviewLayer *previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:_captureSession];
     previewLayer.frame = self.view.bounds;
     [[self.view layer] addSublayer:previewLayer];
     [self.view addSubview:[self createOverlay]];
+    _activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+    _activityIndicator.center = self.view.center;
+    [self.view addSubview:_activityIndicator];
+    [_activityIndicator startAnimating];
 }
 
 - (UIView*)createOverlay {
@@ -107,14 +102,32 @@ static const CGFloat kVerticalInset = 30;
 }
 
 - (void)viewDidLoad {
-    [_rearCamera addObserver:self forKeyPath:@"adjustingFocus" options:NSKeyValueObservingOptionNew context:nil];
-    [self enableButtonIfCameraIsInFocus];
-}
-
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
-    if ([keyPath isEqualToString:@"adjustingFocus"]) {
-        [self enableButtonIfCameraIsInFocus];
-    }
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+        for (AVCaptureDevice *device in [AVCaptureDevice devices]) {
+            if ([device hasMediaType:AVMediaTypeVideo]) {
+                if ([device position] == AVCaptureDevicePositionBack) {
+                    _rearCamera = device;
+                    [_rearCamera lockForConfiguration:nil];
+                    if ([_rearCamera isTorchModeSupported:AVCaptureTorchModeAuto]) {
+                        _rearCamera.torchMode = AVCaptureTorchModeAuto;
+                    } else if ([_rearCamera isFlashModeSupported:AVCaptureFlashModeAuto]) {
+                        _rearCamera.flashMode = AVCaptureFlashModeAuto;
+                    }
+                    [_rearCamera unlockForConfiguration];
+                    [_rearCamera addObserver:self forKeyPath:@"adjustingFocus" options:NSKeyValueObservingOptionNew context:nil];
+                }
+            }
+        }
+        AVCaptureDeviceInput *cameraInput = [AVCaptureDeviceInput deviceInputWithDevice:_rearCamera error:nil];
+        [_captureSession addInput:cameraInput];
+        _stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
+        [_captureSession addOutput:_stillImageOutput];
+        [_captureSession startRunning];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [_activityIndicator stopAnimating];
+            [self enableButtonIfCameraIsInFocus];
+        });
+    });
 }
 
 - (void)enableButtonIfCameraIsInFocus {
